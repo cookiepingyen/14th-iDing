ce616495a0b6c7a39fc6c999582d8e9ebcdd4f50
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-04-23 23:15:35 +0800

    Add restaurants, user model_test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'factory_bot_rails'
+require 'database_cleaner/active_record'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -68,7 +68,6 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
-    Rails.application.load_seed
   end
 
 end
