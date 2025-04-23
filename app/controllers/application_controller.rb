c6da31273f0d14408d748cef0af0e8ce93113a51
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-04-24 00:32:30 +0800

    Update locale, change from tw to zh-TW

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,8 +21,8 @@ class ApplicationController < ActionController::Base
 
   def switch_locale(&action)
     locale = session[:locale] || params[:locale] || I18n.default_locale
-    locale = 'tw' unless I18n.available_locales.include?(locale.to_sym)
-    I18n.with_locale(locale, &action) 
+    locale = :"zh-TW" unless I18n.available_locales.include?(locale.to_sym)
+    I18n.with_locale(locale, &action)
   end
 
   def default_url_options
