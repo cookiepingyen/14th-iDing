cce07ff9c16cc06b1587459d87d0c0b2eb82fe56
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-04-26 17:48:16 +0800

    Update carrierwave setting

--- app/uploaders/menu_uploader.rb
+++ app/uploaders/menu_uploader.rb
@@ -3,12 +3,6 @@
 class MenuUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :fog
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
   process resize_to_fit: [1260, 540]
 
   version :thumb do
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,21 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:               'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_ACCESS_KEY_SECRET"],
-    host: 's3-ap-northeast-1.amazonaws.com',
-    region: 'ap-northeast-1'
-  }
-  config.fog_directory = ENV["AWS_S3_BUCKET_NAME"]
+
+  if Rails.env.development? || Rails.env.test?
+    config.storage :file
+    config.enable_processing = Rails.env.development?
+  else
+    config.storage :fog
+
+    config.fog_credentials = {
+      provider:               'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_ACCESS_KEY_SECRET"],
+      host: 's3-ap-northeast-1.amazonaws.com',
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory = ENV["AWS_S3_BUCKET_NAME"]
+    config.storage_paths = {
+      store_dir: proc { |u| "uploads/#{u.model.class.to_s.underscore}/#{u.mounted_as}/#{u.model.id}" }
+    }
+  end
 end
