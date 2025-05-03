3a1df2d97430eb27e64ce82d0a5c269fcc6afc2a
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-05-03 12:44:26 +0800

    Update carrierwave setting

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,6 +3,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  if Rails.env.production?
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+  end
+
   process resize_to_fit: [1260, 540]
 
   def extension_allowlist
