07fabe68750a56a771e92a28389f663ffd4a8895
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-05-04 08:00:57 +0800

    Add favicon_helper method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "iDing_production"
 
 
-  config.action_mailer.default_url_options = { protocol: 'https', host: 'www.iding.website' }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'iding.website' }
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
-  config.action_mailer.asset_host = 'https://www.iding.website'
+  config.action_mailer.asset_host = 'https://iding.website'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
