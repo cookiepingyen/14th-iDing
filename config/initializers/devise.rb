da12bf8b0b372380cbae65b4b893f41d9eadf809
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-05-07 11:38:36 +0800

    Update mailer email, remove www subdomain

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,7 +16,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'noreply@www.iding.website'
+  config.mailer_sender = 'noreply@iding.website'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'UserMailer'
