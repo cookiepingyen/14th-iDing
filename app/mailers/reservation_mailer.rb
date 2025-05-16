957758ee7dccae79c394d5d47ff3f635193e9834
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-05-16 17:38:43 +0800

    Add reservation_mailer

---
+++ app/mailers/reservation_mailer.rb
@@ -0,0 +1,11 @@
+class ReservationMailer < ApplicationMailer
+  def reservation_email(reservation)
+    @reservation = reservation
+    email = reservation.email
+    restaurant_name = reservation.restaurant.name
+
+    mail(to: email,
+         subject: "您在「#{restaurant_name}」的訂位已確認"
+        )
+  end
+end
